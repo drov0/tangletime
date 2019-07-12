5c37f4c22d8e81dcaecaf7a3af5a0f098fab98a6
Martin Lees <drov0@users.noreply.github.com>
2019-07-12 22:21:26 +0200

    working bluetooth code using 0/1

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,146 +1,83 @@
-#include <SoftwareSerial.h>
-#include <Wire.h>
+/*
+    IR remote keypad
+   0 : FF6897
+   1 : FF18E7
+   2 : FF6897
+   3 : FF7A85
+   4 : FF10EF
+   5 : FF38C7
+   6 : FF5AA5
 
-const int MPU = 0x68; // MPU6050 I2C address
-float AccX, AccY, AccZ;
-float GyroX, GyroY, GyroZ;
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
-float roll, pitch, yaw;
-float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-float elapsedTime, currentTime, previousTime;
-int c = 0;
-
-SoftwareSerial EEBlue(10, 11); // RX | TX
-int data;
-void setup()
-{
-
-  Serial.begin(9600);
-  EEBlue.begin(9600);  //Default Baud for comm, it may be different for your Module.
-  Serial.println("The bluetooth gates are open.\n Connect to HC-05 from any other bluetooth device with 1234 as pairing key!.");
+*/
+/*
+  #include <IRremote.h>
+  #include <SoftwareSerial.h>
+  SoftwareSerial EEBlue(10, 11); // RX | TX
 
 
-  Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);  //end the transmission
-  
-}
+  int RECV_PIN = 12;        // Infrared receiving pin
+  IRrecv irrecv(RECV_PIN); // Create a class object used to receive class
+  decode_results results; // Create a decoding results class object
 
-void loop()
-{
+  void setup()
+  {
+  Serial.begin(9600); // Initialize the serial port and set the baud rate to 9600
+  Serial.println("UNO is ready!");  // Print the string "UNO is ready!"
+  irrecv.enableIRIn(); // Start the receiver
+  EEBlue.begin(9600);
+  }
 
-  /*// Feed any data from bluetooth to Terminal.
+  void loop() {
+  /*EEBlue.write('R');
+  if (irrecv.decode(&results)) {
+    // Waiting for decoding
+    Serial.println(results.value, HEX); // Print out the decoded results
+    EEBlue.write('R');
+    EEBlue.write('\n');
+    EEBlue.write(results.value);
+
+    irrecv.resume(); // Receive the next value
+  }
+
+  // Feed any data from bluetooth to Terminal.
   if (EEBlue.available())
-    Serial.write(EEBlue.read());
+  {
+    Serial.println(EEBlue.read());
+    Serial.println("yesy");
 
+  }
+  /*
   // Feed all data from termial to bluetooth
   if (Serial.available())
-  {
+    EEBlue.write(Serial.read());
 
-    char c  = Serial.read();
+  delay(100);
 
-    EEBlue.write(c);
-    Serial.println(c);
-  }*/
-
-// === Read acceleromter data === //
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-  // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - 0.58; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
-  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + 1.58; // AccErrorY ~(-1.58)
-  // === Read gyroscope data === //
-  previousTime = currentTime;        // Previous time is stored before the actual time read
-  currentTime = millis();            // Current time actual time read
-  elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
-  Wire.beginTransmission(MPU);
-  Wire.write(0x43); // Gyro data first register address 0x43
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 4 registers total, each axis value is stored in 2 registers
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
-  // Correct the outputs with the calculated error values
-  GyroX = GyroX + 0.56; // GyroErrorX ~(-0.56)
-  GyroY = GyroY - 2; // GyroErrorY ~(2)
-  GyroZ = GyroZ + 0.79; // GyroErrorZ ~ (-0.8)
-  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
-  gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
-  gyroAngleY = gyroAngleY + GyroY * elapsedTime;
-  yaw =  yaw + GyroZ * elapsedTime;
-  // Complementary filter - combine acceleromter and gyro angle values
-  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
-  
-  // Print the values on the serial monitor
-  Serial.print(roll);
-  Serial.print("/");
-  Serial.print(pitch);
-  Serial.print("/");
-  Serial.println(yaw);
-
-  
+  }
+*/
+int ledPin = 12;
+int state = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
 }
-
-
-
-void calculate_IMU_error() {
-  // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
-  // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values
-  // Read accelerometer values 200 times
-  while (c < 200) {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3B);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 6, true);
-    AccX = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccY = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    // Sum all readings
-    AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
-    AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
-    c++;
+void loop() {
+  if (Serial.available() > 0) { // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+     Serial.println(state);
   }
-  //Divide the sum by 200 to get the error value
-  AccErrorX = AccErrorX / 200;
-  AccErrorY = AccErrorY / 200;
-  c = 0;
-  // Read gyro values 200 times
-  while (c < 200) {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x43);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 6, true);
-    GyroX = Wire.read() << 8 | Wire.read();
-    GyroY = Wire.read() << 8 | Wire.read();
-    GyroZ = Wire.read() << 8 | Wire.read();
-    // Sum all readings
-    GyroErrorX = GyroErrorX + (GyroX / 131.0);
-    GyroErrorY = GyroErrorY + (GyroY / 131.0);
-    GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);
-    c++;
+
+ 
+  
+  if (state == '0') {
+    digitalWrite(ledPin, LOW); // Turn LED OFF
+    Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
+    state = 0;
   }
-  //Divide the sum by 200 to get the error value
-  GyroErrorX = GyroErrorX / 200;
-  GyroErrorY = GyroErrorY / 200;
-  GyroErrorZ = GyroErrorZ / 200;
-  // Print the error values on the Serial Monitor
-  Serial.print("AccErrorX: ");
-  Serial.println(AccErrorX);
-  Serial.print("AccErrorY: ");
-  Serial.println(AccErrorY);
-  Serial.print("GyroErrorX: ");
-  Serial.println(GyroErrorX);
-  Serial.print("GyroErrorY: ");
-  Serial.println(GyroErrorY);
-  Serial.print("GyroErrorZ: ");
-  Serial.println(GyroErrorZ);
+  else if (state == '1') {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED: ON");;
+    state = 0;
+  }
 }
